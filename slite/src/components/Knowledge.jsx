4e38020ae8c2247d86c5bb9710bb74a60c1bc982
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-07-25 14:11:59 +0100

    knowledge section

--- slite/src/components/Knowledge.jsx
+++ slite/src/components/Knowledge.jsx
@@ -3,7 +3,7 @@ import wiki from "../assets/images/images/wikiframe.png";
 
 const Knowledge = () => {
   return (
-    <div className=" bg-[#F9EFE4] py-10">
+    <div className=" bg-[#F9EFE4] py-10 px-5">
       <section className=" text-center py-12">
         <p className=" font-bold text-2xl">A home for all of your knowledge.</p>
         <span>
@@ -11,12 +11,38 @@ const Knowledge = () => {
           Docs.
         </span>
       </section>
-      <section className="flex mx-12 ">
-        <div className=" w-1/12">
-          <button>Wiki</button>
+      <section className="flex md:mx-12 flex-col md:flex-row gap-5 md:gap-[5rem] items-center justify-center">
+        <div className=" w-full md:w-1/12 flex flex-row md:flex-col md:gap-5 gap-1 flex-wrap px-5">
+          <button className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
+          <button className=" bg-white  w-1/3 md:w-[8rem] h-[3rem] rounded-md ">
+            Wiki
+          </button>
+          <button className=" bg-white w-1/4 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
+          <button className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
+          <button className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
+          <button className=" bg-white w-1/4 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
+          <button className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
+          <button className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
+          <button className=" bg-white w-1/4 md:w-[8rem] h-[3rem] rounded-md p-auto">
+            Wiki
+          </button>
         </div>
         <section className=" bg-frame  bg-no-repeat ">
-          <img src={wiki} alt="" className=" w-[80vw]" />
+          <img src={wiki} alt="" className=" w-[100%]" />
         </section>
       </section>
     </div>
