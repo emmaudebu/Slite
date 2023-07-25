448a990eacc3801d1bcd982e87c77987d8914372
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-07-25 13:23:19 +0100

    knowledge section

--- slite/src/components/Knowledge.jsx
+++ slite/src/components/Knowledge.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import wiki from "../assets/images/images/wikiframe.png";
 
 const Knowledge = () => {
   return (
-    <div className=" bg-[#F9EFE4]">
+    <div className=" bg-[#F9EFE4] py-10">
       <section className=" text-center py-12">
         <p className=" font-bold text-2xl">A home for all of your knowledge.</p>
         <span>
@@ -10,9 +11,13 @@ const Knowledge = () => {
           Docs.
         </span>
       </section>
-      <section>
-        <div></div>
-        <section></section>
+      <section className="flex mx-12 ">
+        <div className=" w-1/12">
+          <button>Wiki</button>
+        </div>
+        <section className=" bg-frame  bg-no-repeat ">
+          <img src={wiki} alt="" className=" w-[80vw]" />
+        </section>
       </section>
     </div>
   );
--- slite/tailwind.config.js
+++ slite/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        frame: "url('../src/assets/images/images/frame.png')",
+      },
+    },
   },
   plugins: [],
 };
