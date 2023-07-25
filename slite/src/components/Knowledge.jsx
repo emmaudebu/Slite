415a8d5ea3affa3b401e47c518a95ded73b5622c
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-07-25 16:53:05 +0100

    images section

--- slite/src/components/Knowledge.jsx
+++ slite/src/components/Knowledge.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import wiki from "../assets/images/images/wikiframe.png";
+import nuro from "../assets/images/images/nurosound.png";
+import creative from "../assets/images/images/creative.png";
 
 const Knowledge = () => {
+  const [Banner, setBanner] = useState("");
+
   return (
     <div className=" bg-[#F9EFE4] py-10 px-5">
       <section className=" text-center py-12">
@@ -13,13 +17,22 @@ const Knowledge = () => {
       </section>
       <section className="flex md:mx-12 flex-col md:flex-row gap-5 md:gap-[5rem] items-center justify-center">
         <div className=" w-full md:w-1/12 flex flex-row md:flex-col md:gap-5 gap-1 flex-wrap px-5">
-          <button className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto shadow-md">
+          <button
+            className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto shadow-md"
+            onClick={() => setBanner(wiki)}
+          >
             Wiki
           </button>
-          <button className=" bg-white  w-1/3 md:w-[8rem] h-[3rem] rounded-md shadow-md ">
+          <button
+            className=" bg-white  w-1/3 md:w-[8rem] h-[3rem] rounded-md shadow-md "
+            onClick={() => setBanner(nuro)}
+          >
             Projects
           </button>
-          <button className=" bg-white w-1/4 md:w-[8rem] h-[3rem] rounded-md p-auto shadow-md">
+          <button
+            className=" bg-white w-1/4 md:w-[8rem] h-[3rem] rounded-md p-auto shadow-md"
+            onClick={() => setBanner(creative)}
+          >
             Processes
           </button>
           <button className=" bg-white w-1/3 md:w-[8rem] h-[3rem] rounded-md p-auto shadow-md">
@@ -42,7 +55,7 @@ const Knowledge = () => {
           </button>
         </div>
         <section className="   bg-no-repeat ">
-          <img src={wiki} alt="" className=" w-[100%]" />
+          <img src={Banner} alt="" className=" w-[100%]" />
         </section>
       </section>
     </div>
